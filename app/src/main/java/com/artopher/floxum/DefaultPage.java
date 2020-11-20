a11658740cbc7b2f43b1da33ab34ffd822e17c4a
Phoenix102001 <adityanair102001@gmail.com>
2020-11-20 13:48:35 +0530

    extended FABs added:-20-11-2020

--- app/src/main/java/com/artopher/floxum/DefaultPage.java
+++ app/src/main/java/com/artopher/floxum/DefaultPage.java
@@ -6,10 +6,26 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DefaultPage extends AppCompatActivity {
+
+     Animation rotateClockwise;
+     Animation rotateAnticlockwise;
+     Animation fabOpen;
+     Animation fabClose;
+
+     FloatingActionButton fab;
+     FloatingActionButton image;
+     FloatingActionButton video;
+     Boolean Clicked = false;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +35,52 @@ public class DefaultPage extends AppCompatActivity {
         BottomNavigationView nav = findViewById(R.id.bottom_nav);
         nav.setOnNavigationItemSelectedListener(navListener);
 
+         rotateClockwise = AnimationUtils.loadAnimation(this,R.anim.rotate_clockwise);
+         rotateAnticlockwise = AnimationUtils.loadAnimation(this,R.anim.rotate_anticlockwise);
+         fabOpen = AnimationUtils.loadAnimation(this,R.anim.fab_open);
+         fabClose = AnimationUtils.loadAnimation(this,R.anim.fab_close);
+
+        final FloatingActionButton fab = findViewById(R.id.fab);
+        final FloatingActionButton image = findViewById(R.id.image);
+        final FloatingActionButton video = findViewById(R.id.video);
+
         getSupportFragmentManager().beginTransaction().replace(R.id.FragmentContainer,new HomeFragment()).commit();
 
+        fab.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                if(!Clicked)
+                {
+                    image.setVisibility(View.VISIBLE);
+                    video.setVisibility(View.VISIBLE);
+                    image.startAnimation(fabOpen);
+                    video.startAnimation(fabOpen);
+                    fab.startAnimation(rotateAnticlockwise);
+                    image.setClickable(true);
+                    video.setClickable(true);
+                    Clicked=true;
+                }
+                else
+                {
+                    image.setVisibility(View.INVISIBLE);
+                    video.setVisibility(View.VISIBLE);
+                    image.startAnimation(fabClose);
+                    video.startAnimation(fabClose);
+                    fab.startAnimation(rotateClockwise);
+                    image.setClickable(false);
+                    video.setClickable(true);
+                    Clicked=false;
+                }
+            }
+        });
+
+
     }
+
+
+
+
     private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener(){
 
 
