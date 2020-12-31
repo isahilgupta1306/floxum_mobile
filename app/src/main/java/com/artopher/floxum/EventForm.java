e0e5a6634dc953bef9fa6264111f523336685664
isahilgupta1306 <sg7544815@gmail.com>
2020-12-31 14:56:58 +0530

    Date Picker , Time Picker added

--- app/src/main/java/com/artopher/floxum/EventForm.java
+++ app/src/main/java/com/artopher/floxum/EventForm.java
@@ -8,15 +8,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
 public class EventForm extends AppCompatActivity {
-    TextInputEditText EventDate , eventTime ;
+    TextInputEditText EventDateEditText , eventTime ;
+    TextInputLayout EventDateTextLayout , EventTimeTextLayout ;
 
     final Calendar myCalendar = Calendar.getInstance();
 
@@ -25,9 +28,12 @@ public class EventForm extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_event_form);
 
-        //Hooks
-        EventDate = findViewById(R.id.event_date);
+        //Hooks of EditText
+        EventDateEditText = findViewById(R.id.event_date);
         eventTime = findViewById(R.id.event_time);
+        //Hooks of Textinput Layout
+        EventDateTextLayout = findViewById(R.id.eventdate);
+        EventTimeTextLayout = findViewById(R.id.eventtime);
 
 
 
@@ -38,11 +44,12 @@ public class EventForm extends AppCompatActivity {
 
 
 
-        EventDate.setOnClickListener(new View.OnClickListener() {
+        EventDateEditText.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
+                Toast.makeText(EventForm.this , "Click succesfull" , Toast.LENGTH_SHORT);
                 Calendar mcurrentTime = Calendar.getInstance();
                 int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
                 int minute = mcurrentTime.get(Calendar.MINUTE);
@@ -50,7 +57,7 @@ public class EventForm extends AppCompatActivity {
                 mTimePicker = new TimePickerDialog(EventForm.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        EventDate.setText( selectedHour + ":" + selectedMinute);
+                        EventDateEditText.setText( selectedHour + ":" + selectedMinute);
                     }
                 }, hour, minute, true);//Yes 24 hour time
                 mTimePicker.setTitle("Select Time");
@@ -81,6 +88,7 @@ public class EventForm extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
+                Toast.makeText(EventForm.this , "Click succesfull" , Toast.LENGTH_SHORT);
                 new DatePickerDialog(EventForm.this, date, myCalendar
                         .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
                         myCalendar.get(Calendar.DAY_OF_MONTH)).show();
