9251b28f9b3d4961f3323f13b4f1e1910bba1a6a
AdityaNair102001 <adityanair102001@gmail.com>
2021-01-13 01:45:34 +0530

    Logout system done.

--- app/src/main/java/com/artopher/floxum/newLoginPage.java
+++ app/src/main/java/com/artopher/floxum/newLoginPage.java
@@ -15,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.artopher.floxum.ApiClasses.ApiClients.ApiClientLogin;
+import com.artopher.floxum.fragments.ProfileFragment;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -127,6 +128,7 @@ public class newLoginPage extends AppCompatActivity {
                           Intent intent = new Intent(getApplicationContext(),DefaultPage.class);
                           saveToken(token);
                           startActivity(intent);
+                          finish(); 
                       }
 
                     }else {
@@ -155,6 +157,7 @@ public class newLoginPage extends AppCompatActivity {
 
         editor.putString(TOKEN,token);
         editor.apply();
+
     }
 
     private String getToken()
